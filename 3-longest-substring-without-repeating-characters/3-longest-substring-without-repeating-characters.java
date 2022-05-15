2aacd7c0872d0106591e7fe25c1961f7c4549962
Zhaokun Xu <98487016+xzkxu@users.noreply.github.com>
2022-05-15 16:27:17 -0400

    Time: 262 ms (7.84%), Space: 248.2 MB (5.40%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Queue<Character> window = new LinkedList<Character>();
+        int result = 0;
+        int length = 0;
+        while(s.length() > 0) {
+            while(window.contains(s.charAt(0))) {
+                window.remove();
+                length--;
+            }
+            window.add(s.charAt(0));
+            length++;
+            if(length>result) {result = length;}
+            s = s.substring(1);
+        }
+        return result;
+    }
+}
