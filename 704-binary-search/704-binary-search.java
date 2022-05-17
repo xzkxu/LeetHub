50d8d2a25a949de02a6f524a792e6deeeb5d87df
Zhaokun Xu <98487016+xzkxu@users.noreply.github.com>
2022-05-17 00:10:42 -0400

    Time: 0 ms (100.00%), Space: 54.5 MB (32.18%) - LeetHub

---
+++ 704-binary-search/704-binary-search.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length;
+        while (right > left) {
+            int pivot = left + ((right - left) >> 1);
+            if(nums[pivot] == target) { 
+                return pivot;
+            }
+            if(nums[pivot] < target) {
+                left = pivot+1;
+            }
+            if(nums[pivot] > target) {
+                right = pivot;
+            }
+        }
+        return -1;
+    }
+}
